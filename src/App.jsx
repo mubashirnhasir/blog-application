efdd1a6568bdaec89f4d73db29016ba3199a7328
mubashirhasirnisar <mubashirhasirnisar@gmail.com>
2025-02-11 14:04:39 +0000

    appwrite authentication

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <div className=''>
+            Mubahsir nisar
+      </div>
+    </>
+  )
+}
+
+export default App
